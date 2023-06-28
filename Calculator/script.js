2d5efdb2ba4cd77d818d5af9716d881602094f9b
Deepak Chaudhary <68501527+DeepakChaudhary47@users.noreply.github.com>
2023-06-29 01:05:50 +0530

    Add files via upload
    This is a simple Calculator using HTML,CSS and javaScript.

---
+++ Calculator/script.js
@@ -0,0 +1,104 @@
+
+const input = document.querySelector("#input")
+const output = document.querySelector("#output")
+const keyContainer = document.querySelector(".keyboard")
+
+// Global variables declaretion that manages states globally
+let val = []
+let operator
+let currNum = []
+
+// Adding EvenentListener on click event
+keyContainer.addEventListener("click", (e) => {
+    // Assigning e.target to currObj
+    currObj = e.target
+
+    // Checking if user press operator
+    if(currObj.id == 'op'){
+
+        // checking obj if user clicks on clear button
+        if(currObj.innerText === 'C'){
+            val = []
+            operator = ''
+            currNum = []
+            output.innerText = 0
+            input.value = [0]
+            return
+        }
+        // checking if user clicks on backspace
+        if(currObj.innerText === 'Z'){
+            backSpace()
+            return
+        }
+
+
+        // checking if user want result
+        if(currObj.innerText === '=')
+        {
+            if(currNum!=null && currNum > 0){
+                // Updating state on input box
+                inputState(val, operator, currNum)
+
+                // updating input array
+                val.push(Number(currNum))
+                currNum = []
+            }
+            // Calling Calculate function
+            if(val.length >1){
+                calCulate(val, operator)
+            }
+            return
+        }
+
+        // Assigning Operator into operator variable
+        operator = currObj.innerText
+        if(val.length <=1){
+            input.value = [...val, currNum, operator].join("")
+        }
+
+        // checking if currNum is not null
+        if(currNum!=null && currNum > 0){
+            val.push(Number(currNum))
+            currNum = []
+            return
+        }
+    }
+    else{
+        // if user wants to enter contegeous numbers or single number
+        currNum += currObj.innerText
+
+        // changing input box states
+        inputState(val, operator, currNum)
+    }
+})
+
+// Definition of calCulate function
+const calCulate = (val, operator) => {
+    let firstNum = val[0]
+    let nextNum = val[1]
+
+    // Evaluations of current statement
+    nextNum = eval(`${firstNum} ${operator} ${nextNum}`)
+
+    // Updating Next Number Value
+    val[0] = nextNum
+    val.pop()
+
+    // Updating output onScreen
+    output.innerText = nextNum
+    return
+}
+
+// Definition of inputState function
+const inputState = (val, operator, currNum) =>{
+    input.value = [val,operator, currNum].join("")
+}
+
+// Definition of backSpace function
+const backSpace = () => {
+   
+    let temp = input.value
+    input.value = temp.slice(0, -1)
+    currNum = currNum.slice(0, -1)
+
+}
